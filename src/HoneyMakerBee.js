614c90ff8932f56f71edf55ee4a86880f523425a
Johnny <po_bo@hotmail.com>
2017-01-06 11:17:27 -0800

    completed all sub classing

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor (age, color, food, job) {
+    super(age, color, food, job);
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+
+  makeHoney () {
+    return this.honeyPot += 1;
+  }
+
+  giveHoney () {
+    return this.honeyPot -=1;
+  }
 };
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,17 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor (age, color, food, job) {
+    super(age, color, food, job);
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+  }
+
+  forage () {
+    return "I am too old, let me play cards instead";
+  }
+
+  gamble (treasure) {
+    this.treasureChest.push(treasure)
+  }
 };
